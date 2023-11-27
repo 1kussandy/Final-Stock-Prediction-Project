3e1c00e249dfad9718ffb028d097737fd4cb95c7
1kussandy <63507542+1kussandy@users.noreply.github.com>
2023-11-27 14:09:26 -0500

    Update CNN.py
    Added the accuracy as well as some test cases also trained data of Stock AAPL

--- CNN.py
+++ CNN.py
@@ -1,31 +1,48 @@
 import numpy as np
-import tensorflow as tf
+from sklearn.metrics import mean_squared_error
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense
-from sklearn.metrics import mean_squared_error
 
-# Assuming X_train, X_test, y_train, y_test are obtained from previous steps
+def cnn_model(X_train, X_test, y_train, y_test):
+    cnn_model = Sequential()
+    cnn_model.add(Conv1D(filters=32, kernel_size=3, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])))
+    cnn_model.add(MaxPooling1D(pool_size=1))  # Updated pool_size to 1
 
-# Reshaping the data for CNN (Adding a channel dimension)
-X_train_cnn = np.expand_dims(X_train.values, axis=-1)
-X_test_cnn = np.expand_dims(X_test.values, axis=-1)
+    cnn_model.add(Flatten())
+    cnn_model.add(Dense(50, activation='relu'))
+    cnn_model.add(Dense(1))
 
-# CNN Model Architecture
-cnn_model = Sequential([
-    Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(X_train_cnn.shape[1], 1)),
-    MaxPooling1D(pool_size=2),
-    Flatten(),
-    Dense(50, activation='relu'),
-    Dense(1)  # Output layer (for regression)
-])
+    cnn_model.compile(optimizer='adam', loss='mean_squared_error')
+    cnn_model.fit(X_train, y_train, epochs=10, batch_size=32, verbose=1)
+    
+    cnn_predictions = cnn_model.predict(X_test)
+    
+    cnn_rmse = np.sqrt(mean_squared_error(y_test, cnn_predictions))
 
-# Compile the CNN model
-cnn_model.compile(optimizer='adam', loss='mean_squared_error')
+    # Calculate RMSE
+    rmse = np.sqrt(np.mean(np.square(cnn_predictions - y_test)))
 
-# Train the CNN model
-cnn_model.fit(X_train_cnn, y_train, epochs=10, batch_size=32, verbose=1)
+    # Calculate the range of the target variable (replace min and max with your actual values)
+    min_target_value = np.min(y_test)
+    max_target_value = np.max(y_test)
+    target_range = max_target_value - min_target_value
 
-# Evaluate the CNN model on the test dataset
-cnn_predictions = cnn_model.predict(X_test_cnn)
-cnn_rmse = np.sqrt(mean_squared_error(y_test, cnn_predictions))
-print(f"CNN Model RMSE on Test Set: {cnn_rmse}")
+    # Calculate accuracy in percentage
+    accuracy_percentage = ((1 - (rmse / target_range)) * 100).round(2)
+
+    print(f"CNN Model RMSE on Test Set: {cnn_rmse}")
+    print(f"Accuracy Percentage: {accuracy_percentage}%")
+
+    return cnn_predictions
+
+# Test the CNN model with dummy data
+if __name__ == "__main__":
+    # Dummy data for testing purposes
+    np.random.seed(42)
+    X_train_dummy = np.random.rand(100, 3, 2)  # Example shape for X_train
+    y_train_dummy = np.random.rand(100,)  # Example shape for y_train
+    X_test_dummy = np.random.rand(20, 3, 2)  # Example shape for X_test
+    y_test_dummy = np.random.rand(20,)  # Example shape for y_test
+
+    # Call CNN model function with dummy data
+    cnn_predictions_dummy = cnn_model(X_train_dummy, X_test_dummy, y_train_dummy, y_test_dummy)
