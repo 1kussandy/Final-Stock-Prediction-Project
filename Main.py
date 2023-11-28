ce6390a06ba2ed73b3960e1e5a51459f81fcb02c
1kussandy <63507542+1kussandy@users.noreply.github.com>
2023-11-28 18:09:41 -0500

    Update Main.py
    added dataset

--- Main.py
+++ Main.py
@@ -1,45 +1,35 @@
-import yfinance as yf
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
-# Step 1: Task & Data Selection - Obtaining historical stock price data using yfinance
-ticker_symbol = 'AAPL'  # Apple Inc. ticker symbol
-start_date = '2010-01-01'  # Start date for historical data retrieval
-end_date = '2022-01-01'  # End date for historical data retrieval
+def preprocess_data(data_path):
+    # Load the stocks dataset
+    stocks_data = pd.read_csv(data_path)  # Replace 'stocks.csv' with your file path
 
-# Fetch historical stock price data using yfinance
-stock_data = yf.download(ticker_symbol, start=start_date, end=end_date)
+    # Perform preprocessing on the stocks dataset
+    # Assuming 'symbol', 'date', 'open', 'high', 'low', 'close', and 'volume' columns are present
+    # Extracting 'symbol' and 'date' as non-numeric features, and other columns as numeric features
+    X = stocks_data[['open', 'high', 'low', 'volume']]  # Features (numeric columns)
+    y = stocks_data['close']  # Target variable
 
-# Step 2: Preprocessing
-# Data Cleaning: Handling missing values
-stock_data.dropna(inplace=True)
+    # Normalize/Scale numerical features for model training
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(X)
 
-# Feature Engineering: Adding a 50-day Moving Average as a new feature
-stock_data['50_MA'] = stock_data['Close'].rolling(window=50).mean()
+    # Splitting the data into training and testing sets (80% train, 20% test)
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-# Normalization/Scaling: Scale numerical features (Close and 50_MA) for model training
-scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(stock_data[['Close', '50_MA']])
-scaled_df = pd.DataFrame(scaled_data, columns=['Close_Scaled', '50_MA_Scaled'])
+    return X_train, X_test, y_train, y_test
 
-# Concatenating scaled features with the original dataset
-stock_data = pd.concat([stock_data, scaled_df], axis=1)
+if __name__ == "__main__":
+    data_path = 'stocks.csv'  # Replace with your dataset path
+    X_train, X_test, y_train, y_test = preprocess_data(data_path)
+    print("Preprocessed Data Shapes:")
+    print("X_train shape:", X_train.shape)
+    print("X_test shape:", X_test.shape)
+    print("y_train shape:", y_train.shape)
+    print("y_test shape:", y_test.shape)
 
-# Train-Test Split: Divide data into training and testing sets
-# Considering 'Close' as the target variable for prediction
-X = stock_data[['Close_Scaled', '50_MA_Scaled']]  # Features
-y = stock_data['Close']  # Target variable
+    # Save preprocessed data if needed
+    # ...
 
-# Splitting the data into training and testing sets (80% train, 20% test)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Displaying the preprocessed data and the split sets
-print("Preprocessed Stock Data:")
-print(stock_data.head())
-
-print("\nTraining and Testing Set Sizes:")
-print("X_train shape:", X_train.shape)
-print("X_test shape:", X_test.shape)
-print("y_train shape:", y_train.shape)
-print("y_test shape:", y_test.shape)
